3c88fa9fe29c4d953c32da974e65f636b15cf08a
Matias Héctor Sampieri <89465@sistemas.frc.utn.edu.ar>
2024-08-26 16:00:12 -0300

    funciona

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let sabe = true
 
 function saveData(data) {
-    $.post("https://52e5imxbftoafjuq22sekscfci0fqcuy.lambda-url.sa-east-1.on.aws/emissions", data)
+    $.ajax({
+        type: 'POST',
+        url: 'https://52e5imxbftoafjuq22sekscfci0fqcuy.lambda-url.sa-east-1.on.aws/emissions',
+        data: JSON.stringify(data),
+        dataType: 'text',
+        contentType: 'text/plain'
+    })
 }
 
 $(document).ready(function () {
@@ -23,16 +29,15 @@ $(document).ready(function () {
             watts = '80'
         }
 
-        const co2 = parseInt(watts) * 60 * 52 * 0.0003966
+        watts = parseInt(watts)
+        const co2 = watts * 60 * 52 * 0.0003966
         
         $('#txt-resultado').text(`Tu computadora consume ${co2} TCO2e al año`)
         $('#resultados').prop('hidden', false)
 
-        saveData({
-            type,
-            watts,
-            co2
-        })
+
+        saveData({type, watts, co2})
+        //$.get("https://52e5imxbftoafjuq22sekscfci0fqcuy.lambda-url.sa-east-1.on.aws/emissions").then(z=>console.log(z))
     })
 })
 
